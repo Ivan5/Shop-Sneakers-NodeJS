294a3df03c35e48bca477a7cacccda7b19dbe78c
Ivan5 <converse_ivan@hotmail.com>
2018-06-29 18:31:45 -0500

    Handling Errors

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
+app.use(morgan('dev'));
 const sneakerRoutes = require('./api/routes/sneakers');
 const orderRoutes = require('./api/routes/orders')
 app.use('/sneakers', sneakerRoutes);
 app.use('/orders',orderRoutes);
+
+app.use((req,res,next)=>{
+  const error = new Error('Not Found');
+  error.status(404);
+  next(error);
+});
+
+app.use((error,req,res,next)=>{
+  res.status(error.status || 500);
+  res.json({error:{
+    message: error.mesage
+  }});
+})
 module.exports = app;
