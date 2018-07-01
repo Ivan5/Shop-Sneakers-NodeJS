dec4962ec281cbb667ed52ca42fa2e8f37bb350e
Ivan5 <converse_ivan@hotmail.com>
2018-06-30 20:11:07 -0500

    Managing orders with Mongoose

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,17 +1,55 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Order = require('../models/order')
 
 router.get('/',(req,res,next)=>{
-  res.status(200).json({
-    message: 'Orders were fetched'
-  })
+  Order.find()
+    .select('sneaker quantity _id')
+    .exec()
+    .then(docs => {
+      res.status(200).json({
+        count: dosc.length,
+        orders: docs.map(doc => {
+          return {
+            _id:doc.id,
+            sneaker: doc.sneaker,
+            quantity: doc.quantity,
+            request:{
+              type: 'GET',
+              url: 'http://localhost:3000/orders/'+doc._id
+            }
+          }
+        }),
+        
+      })
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      })
+    })
 });
 
 router.post('/',(req,res,next)=>{
-  const order = {
-    sneakerId: req.body.sneakerId,
-    quantity: req.body.quantity 
-  }
+  const order = new Order({
+    _id: mongoose.Types.ObjectId(),
+    quantity: req.body.quantity,
+    sneaker: req.body.sneakerId
+  });
+  order.save()
+    .then(result => {
+      console.log(result);
+      res.status(200).json(result)
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error:err
+      })
+      
+    })
   res.status(200).json({
     message: 'Orders was created',
     order
@@ -19,19 +57,29 @@ router.post('/',(req,res,next)=>{
 });
 
 router.get('/:orderId',(req,res,next)=>{
-  const id = req.param.orderId;
-  res.status(200).json({
-    message: 'Order details',
-    id
-  })
+  Order.findById(req.params.orderId).exec()
+    .then(order => {
+      res.status(200).json({
+        order: order
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      })
+    })
 });
 
 router.delete('/:orderId',(req,res,next)=>{
-  const id = req.param.orderId;
-  res.status(200).json({
-    message: 'Order deleted',
-    id
-  })
+  Order.remove({_id: req.params.orderId}).exec()
+    .then(result => {
+      message: 'Order deleted'
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      })
+    })
 });
 
 module.exports = router;
